51e4d440eac7d97c4b93fda04067828fa5d64d2c
rubyrain44 <rubyrain44@yahoo.com>
2023-07-02 15:08:18 -0400

    first commit

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,62 @@
+from flask import render_template, redirect, request, session, flash
+from flask_app import app
+from flask_bcrypt import Bcrypt 
+bcrypt = Bcrypt(app)
+from flask_app.models.user import User
+
+
+#INDEX
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#LOGOUT
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.clear()
+    return redirect ('/') 
+
+
+#REGISTER FORM
+@app.route('/register', methods=['POST'])
+def register():
+    if not User.validate_user(request.form):
+        return redirect('/')
+    pw_hash = bcrypt.generate_password_hash(request.form['register_password'])
+    print(pw_hash)
+    data = {
+        'first_name' : request.form['first_name'],
+        'last_name' : request.form['last_name'],
+        'email' : request.form['email'],
+        'password' : pw_hash
+    }
+    session['user_id'] = User.create_user(data)
+    print(session['user_id'])
+    return redirect ('/dashboard')
+
+
+#LOGIN CURRENT USER FORM
+@app.route('/login', methods=['POST'])
+def login():
+    data = { "email" : request.form["email"] }
+    user_in_db = User.get_email(data)
+    if not user_in_db:
+        flash("Invalid Email/Password")
+        return redirect("/")
+    if not bcrypt.check_password_hash(user_in_db.password, request.form['login_password']):
+        flash("Invalid Email/Password")
+        return redirect('/')
+    session['user_id'] = user_in_db.id
+    return redirect("/dashboard")
+
+
+#SUCCESSFUL REDIRECT TO DASHBOARD#
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' in session:
+        user = User.get_user({'id' : session['user_id']})
+        return render_template ('dashboard.html', user=user)
+    return redirect ('/')
+
+
+
